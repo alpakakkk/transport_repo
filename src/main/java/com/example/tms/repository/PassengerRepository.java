19bdcba54536b603477c18fcf274dd3699ea1fe8
alpakakkk <almanzork270208@gmail.com>
2026-02-10 13:00:57 +0500

    Update PassengerRepository.java

--- src/main/java/com/example/tms/repository/PassengerRepository.java
+++ src/main/java/com/example/tms/repository/PassengerRepository.java
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 package com.example.tms.repository;
 
 import com.example.tms.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-=======
-package com.example.transport.repository;
-
-public class PassengerRepository {
->>>>>>> edf3098242461726d6e47f864ae7dc1a0d1bba05
 }

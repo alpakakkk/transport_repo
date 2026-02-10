1ac12af612a02635ef5ad2bfff4d01313d62224c
alpakakkk <almanzork270208@gmail.com>
2026-02-10 13:00:42 +0500

    Update BusRepository.java

--- src/main/java/com/example/tms/repository/BusRepository.java
+++ src/main/java/com/example/tms/repository/BusRepository.java
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 package com.example.tms.repository;
 
 import com.example.tms.entity.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BusRepository extends JpaRepository<Bus, Long> {
-=======
-package com.example.transport.repository;
-
-public class BusRepository {
->>>>>>> edf3098242461726d6e47f864ae7dc1a0d1bba05
 }
